72b16cf364e63368c29c0b5210c0c8130ca8025e
Yuva-raghav <yuva1212k@gmail.com>
2026-03-09 18:27:28 +0530

    two_sum
    Refactor two_sum function to twoSum and optimize using a dictionary for index lookup. Remove user input and provide a sample input for testing.

--- python learning/leetcode/two_sum.py
+++ python learning/leetcode/two_sum.py
@@ -1,19 +1,12 @@
-# Function to find the positions of the two numbers
-def two_sum(nums, target):
-    for i in range(len(nums)): # Look at first number
-        for j in range(i + 1, len(nums)): # Look at second number
-            if nums[i] + nums[j] == target: # Check if they match the target
-                return [i, j] # Return their positions
+def twoSum(nums, target):
+    num_to_index = {} 
 
-# 1. Ask user for a list of numbers
-user_nums = input("Enter numbers separated by space: ")
+    for i, num in enumerate(nums):
+        if target - num in num_to_index:
+            return [num_to_index[target - num], i]
+        num_to_index[num] = i
 
-# Convert the text into a list of integers
-nums = [int(x) for x in user_nums.split()]
-
-# 2. Ask user for the target number
-user_target = int(input("Enter target number: "))
-
-# 3. Call the function and print the result
-output = two_sum(nums, user_target)
-print("Output:", output)
+    return [] 
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))  
