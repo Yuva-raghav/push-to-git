7cd35f9ed1e066f06eae13a32ecd4f7a88c8c549
Yuva <yuva1212k@gmail.com>
2026-03-10 18:09:10 +0530

    Restore user's original solution code for merge_two_sorted_lists

--- python learning/leetcode/merg_two_sorted_lists.py
+++ python learning/leetcode/merg_two_sorted_lists.py
@@ -3,36 +3,25 @@ class ListNode:
         self.val = val
         self.next = next
 
-def mergeTwoLists(list1, list2):
+def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
     dummy = ListNode()
-    current = dummy
+    tail = dummy
     
     while list1 and list2:
         if list1.val < list2.val:
-            current.next = list1
+            tail.next = list1
             list1 = list1.next
         else:
-            current.next = list2
+            tail.next = list2
             list2 = list2.next
-        current = current.next
-        
-    if list1:
-        current.next = list1
-    elif list2:
-        current.next = list2
-        
+        tail = tail.next
+    
+    # Attach the remaining nodes
+    tail.next = list1 if list1 else list2
+    
     return dummy.next
 
-# Helper function to print linked list
-def print_list(node):
-    result = []
-    while node:
-        result.append(node.val)
-        node = node.next
-    print(result)
 
-# Example usage:
-list1 = ListNode(1, ListNode(2, ListNode(4)))
-list2 = ListNode(1, ListNode(3, ListNode(4)))
-merged_list = mergeTwoLists(list1, list2)
-print_list(merged_list)
+# Example usage
+# list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
