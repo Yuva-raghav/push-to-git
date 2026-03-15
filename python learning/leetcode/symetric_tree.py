179312cf9332fd7e2f5887d5d305310368664558
Yuva <yuva1212k@gmail.com>
2026-03-15 08:22:20 +0530

    Add leetcode solutions: Valid Parentheses, Remove Duplicates from Sorted Array, Palindrome, Find Index of First Occurrence, Sqrt(x), Symmetric Tree, update Maximum Depth of Binary Tree

---
+++ python learning/leetcode/symetric_tree.py
@@ -0,0 +1,14 @@
+def is_symmetric(root):
+    if not root:
+        return True
+
+    def is_mirror(t1, t2):
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+        return (t1.val == t2.val and 
+                is_mirror(t1.left, t2.right) and 
+                is_mirror(t1.right, t2.left))
+
+    return is_mirror(root.left, root.right)
