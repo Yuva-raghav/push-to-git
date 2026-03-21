ada2294d18dfd95c1b0b96edacc1035c1cab4058
Yuva <yuva1212k@gmail.com>
2026-03-21 20:48:47 +0530

    added Binary Tree Inorder Traversal.py

---
+++ python learning/leetcode/Binary Tree Inorder Traversal.py
@@ -0,0 +1,9 @@
+def inorder_traversal(root):
+    result = []
+    def inorder(node):
+        if node:
+            inorder(node.left)
+            result.append(node.val)
+            inorder(node.right)
+    inorder(root)
+    return result
