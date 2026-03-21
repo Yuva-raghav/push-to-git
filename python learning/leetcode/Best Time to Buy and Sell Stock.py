de897d2476a50a7141cc7817fdc304a6cf76e37e
Yuva <yuva1212k@gmail.com>
2026-03-21 20:48:47 +0530

    added Best Time to Buy and Sell Stock.py

---
+++ python learning/leetcode/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,15 @@
+def max_profit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+
+    return max_profit
+
+# Example usage
+print(max_profit([7,1,5,3,6,4]))  # Output: 5
+print(max_profit([7,6,4,3,1]))    # Output: 0
